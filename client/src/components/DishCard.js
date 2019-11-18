de225d8df2869443e9a70c52373a78a87c056199
ABCbum <kimlong221002@gmail.com>
2019-11-18 22:13:35 +0700

    Add basic change language features to each DishCard; Refactor DishCard into smaller components

--- client/src/components/DishCard.js
+++ client/src/components/DishCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 import './css/DishCard.css'
+import DishCardContent from './DishCardContent';
 
 class DishCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      lang: 'en'
+    };
   }
 
   async getNewDish() {
@@ -16,38 +19,30 @@ class DishCard extends React.Component {
 
   async setNewDish() {
     const newDish = await this.getNewDish();
-    this.setState(newDish);
+    this.setState({dish: newDish});
   }
 
-  listify(ingredients) {
-    if(ingredients) {
-      if(!ingredients[ingredients.length - 1]) ingredients.pop();
-      const ingredientsList = ingredients
-                            .map(ingredient => (<li>{ingredient}</li>));
-      return ingredientsList
-    }
-    return 
+  changeLang(lang) {
+    if(this.state.lang !== lang) this.setState({lang});
   }
 
-  // TODO: Add conditional rendering
   render() {
-    let on = Boolean(this.state.name);
+    let lang = this.state.lang;
+    let hasContent = Boolean(this.state.dish);
+    let dish = hasContent ? this.state.dish : null;
+    
     return (
       <div className="DishCard" type={`${this.props.type}_dish_card`}>
-        <div>
-          <h2>{this.state.name}</h2>
-          <h3>{on ? '~Ingredients~' : null}</h3>
-          <ol>
-            {this.listify(this.state.ingredients)}
-          </ol>
-          <h3>{on ? '~Steps~' : null}</h3>
-          <ol> 
-            {this.listify(this.state.steps)}
-          </ol>
-          <h3>{on ? '~Ready time~' : null}</h3><p>{on ? `${this.state.readyTime}` : null}</p>
-          <h3>{on ? '~Nutrition~:' : null}</h3><p>{on ? `${this.state.nutrition}` : null}</p>
-        </div>
-
+        {hasContent && 
+          <DishCardContent 
+            content={dish}
+            lang={lang}
+          />
+        }
+      <div>
+        {hasContent && <a className="changeLang" onClick={this.changeLang.bind(this, 'vi')}>VI</a>}
+        {hasContent && <a className="changeLang" onClick={this.changeLang.bind(this, 'en')}>EN</a>}
+      </div>
         <button className="button button1" onClick={this.setNewDish.bind(this)}> 
           Get new {this.props.type}
         </button>
